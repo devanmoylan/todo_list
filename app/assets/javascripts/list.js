d7a2e9a60c1f15a402e843eaf1cb20b3339c67e7
Devan <devan.moylan@backyardlion.com>
2017-07-30 21:28:09 -0600

    addition of the completed? toggle for items

---
+++ app/assets/javascripts/list.js
@@ -0,0 +1,7 @@
+
+
+$(function(){
+    $("input.toggle").on("change", function(){
+        $(this).parents("form").trigger("submit")
+    })
+});
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,9 +1,22 @@
 class ItemsController < ApplicationController
+  before_action :set_item, only: [:update]
+  def new
+  end
+
+  def update
+    if @item.update(item_params)
+      redirect_to list_path(@item.list)
+    end
+  end
 
   private
 
+  def set_item
+    @item = Item.find(params[:id])
+  end
+
   def item_params
-    params.require(:item).permit(:name)
+    params.require(:item).permit(:name, :status)
   end
 
 end
