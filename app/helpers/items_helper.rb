d1e54d961e74edbbcc72cbca014f0742a7b7db88
Devan <devan.moylan@backyardlion.com>
2017-07-31 10:13:47 -0600

    Refactoring the view, adding helper methods for the item

--- app/helpers/items_helper.rb
+++ app/helpers/items_helper.rb
@@ -1,2 +1,12 @@
 module ItemsHelper
+
+  def li_class_for_item(item)
+    "completed" if item.completed?
+  end
+
+  def form_for_item_status(item)
+    form_for ([item.list, item]) do |f|
+      f.check_box :status, :class => "toggle", :checked => item.completed?
+    end
+  end
 end
